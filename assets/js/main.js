dfe60dfd1bb4f1b18a7e97fc9b2aa600dc636377
carlos738 <carlosasl135@hotmail.com>
2023-11-27 02:12:22 -0400

    pokedex-1

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class></li>
+    `
+}
